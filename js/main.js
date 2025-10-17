addee9ab9f5b367c220bf1b9158d7675624574c8
Sanchaai <sanchaai.mathi@gmail.com>
2025-10-17 15:32:29 -0400

    Adapt worldmap into class

---
+++ js/main.js
@@ -0,0 +1,65 @@
+
+// Variables for the visualization instances
+let worldMap, timeline;
+
+
+// Start application by loading the data
+loadData();
+
+function loadData() {
+    // d3.json("data/"). then(jsonData=>{
+            
+    //     // prepare data
+    //     let data = prepareDataForStudents(jsonData)
+        
+    //     console.log('data loaded ')
+
+    //     // TO-DO (Activity I): instantiate visualization objects
+		areachart = new WorldMap("world-map") //, data.layers)
+		timeline = new Timeline("timeline")
+
+    //     // TO-DO (Activity I):  init visualizations
+		areachart.initVis()
+		timeline.initVis()
+    // });
+}
+
+
+// helper function - PROVIDE WITH TEMPLATE
+function prepareDataForStudents(data){
+
+	// let parseDate = d3.timeParse("%Y");
+
+	// let preparedData = {};
+
+	// // Convert Pence Sterling (GBX) to USD and years to date objects
+	// preparedData.layers = data.layers.map( d => {
+	// 	for (let column in d) {
+	// 		if (d.hasOwnProperty(column) && column !== "Year") {
+	// 			d[column] = parseFloat(d[column]) * 1.481105 / 100;
+	// 		} else if(d.hasOwnProperty(column) && column === "Year") {
+	// 			d[column] = parseDate(d[column].toString());
+	// 		}
+	// 	}
+	// });
+
+	// //
+	// data.years.forEach(function(d){
+	// 	d.Expenditures = parseFloat(d.Expenditures) * 1.481105 / 100;
+	// 	d.Year = parseDate(d.Year.toString());
+	// });
+
+	// return data
+}
+
+
+
+function brushed() {
+	// // Get the extent of the current brush
+	// let selection = d3.brushSelection(d3.select(".brush").node());
+	
+	// // Convert the extent into the corresponding domain values
+	// areachart.x.domain(selection.map(timeline.x.invert))
+
+	// areachart.wrangleData()
+}
